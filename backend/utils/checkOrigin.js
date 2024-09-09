2840be2a1ca3c2d66c94c838eaf2a13aa2da0f85
Hnq <hancu345@gmail.com>
2024-09-09 12:01:13 +0300

    final v3

--- backend/utils/checkOrigin.js
+++ backend/utils/checkOrigin.js
@@ -1,7 +1,11 @@
 function checkOrigin(req, res, next) {
   const origin = req.get("origin") || req.get("referer");
 
-  if (origin && origin.includes("http://localhost:3000")) {
+  if (
+    origin &&
+    (origin.includes("http://localhost:3000") ||
+      origin.includes("https://find-your-lost-pet.vercel.app"))
+  ) {
     next();
   } else {
     res.status(403).json({ message: "Access denied: Invalid origin" });
